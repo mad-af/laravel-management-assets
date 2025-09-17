6f393f3587c88c0119d6fa340c45643b278ac0ad
Mochamad Aufa <mochamadaufa7@gmail.com>
2025-09-17 13:26:24 +0700

    refactor(asset-transfers): remove unused asset transfer components

--- resources/views/dashboard/asset-transfers/index.blade.php
+++ resources/views/dashboard/asset-transfers/index.blade.php
@@ -6,7 +6,5 @@
     <livewire:dashboard-content-header title='Asset Transfer Management' description='Kelola transfer aset antar lokasi dalam sistem.'
         buttonText='Create Transfer' buttonIcon='o-plus' buttonAction='openAssetTransferDrawer' />
 
-    <livewire:asset-transfers.table />
 
-    <livewire:asset-transfers.drawer />
 @endsection
